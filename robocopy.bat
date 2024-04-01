a4c0acca44d7c5bac090d4e22dbbab5782e1f100
Louis <88962972+LouisLee985@users.noreply.github.com>
2024-04-01 09:15:45 +0800

    Update robocopy.bat

--- robocopy.bat
+++ robocopy.bat
@@ -1,5 +1,15 @@
 @echo off
+setlocal enabledelayedexpansion
 set "src=%~dp0python"
 set "dst=%~dp0InteractiveHtmlBom\InteractiveHtmlBom"
-robocopy "%src%" "%dst%" /e
-pause
+cd /d "%src%"
+copy /y "%src%\*.*" "!dst!\"
+for /f "delims=" %%i in ('dir /b /s /ad "%src%"') do (
+    set "NewFolder=%%i"
+    set "NewFolder=%dst%!NewFolder:%src%=!"
+    if not exist "!NewFolder!" (
+        md "!NewFolder!"
+    )
+    copy /y "%%i\*.*" "!NewFolder!\"
+)
+rem pause

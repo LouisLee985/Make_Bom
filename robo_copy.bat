7e37bb9c32e2c37de90cbcc3c8f4f1eed4eefaab
Louis <88962972+LouisLee985@users.noreply.github.com>
2024-04-01 10:38:20 +0800

    Update robo_copy.bat

--- robo_copy.bat
+++ robo_copy.bat
@@ -1,7 +1,7 @@
 @echo off
 set "src=%~dp0python"
 set "dst=%~dp0InteractiveHtmlBom\InteractiveHtmlBom"
-robocopy %~dp0 %dst% "*.ini" /mt /z
+robocopy %~dp0 "%dst%" "*.ini" /mt /z
 robocopy "%src%" "%dst%" /e
 pause
 del "%~dp0*_*.bat"

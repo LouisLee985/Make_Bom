78e25547a81d43819685ad9318bfd47d5da9197f
Louis <88962972+LouisLee985@users.noreply.github.com>
2024-04-02 10:36:52 +0800

    Update rightclick.bat

--- rightclick.bat
+++ rightclick.bat
@@ -2,9 +2,9 @@
 set "rname=MakeBom"
 set "bpath=%~dp0makebom.bat"
 
-REG ADD HKEY_CLASSES_ROOT\*\shell\%rname% 					/t REG_SZ        /d %rname% /f
-REG ADD HKEY_CLASSES_ROOT\*\shell\%rname%			        /t REG_EXPAND_SZ /v "Icon" /d \"%~dp0bomicon.ico\" /f
-REG ADD HKEY_CLASSES_ROOT\*\shell\%rname%\Command 			/t REG_SZ        /d "\"%bpath%\" \"%%1\"" /f
+REG ADD HKEY_CLASSES_ROOT\*\shell\%rname%             /t REG_SZ        /d %rname% /f
+REG ADD HKEY_CLASSES_ROOT\*\shell\%rname%             /t REG_EXPAND_SZ /v "Icon" /d \"%~dp0bomicon.ico\" /f
+REG ADD HKEY_CLASSES_ROOT\*\shell\%rname%\Command     /t REG_SZ        /d "\"%bpath%\" \"%%1\"" /f
 
 REG ADD HKEY_CURRENT_USER\Software\Classes\json_auto_file\shell\open\command  /t REG_SZ /d "\"%bpath%\" \"%%1\"" /f
 
